3bd2716a655b8068ee804240f7db62651098681d
Mahesh <mahesh@altersense.in>
2014-11-24 01:42:52 +0530

    realizing design task panels beautifuly inflated. SubHeader Panel being done.

--- src/android/src/in/altersense/taskapp/TasksAtHand.java
+++ src/android/src/in/altersense/taskapp/TasksAtHand.java
@@ -1,16 +1,36 @@
 package in.altersense.taskapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class TasksAtHand extends Activity {
+	
+	private LinearLayout tasksAtHand;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tasks_at_hand);
+		
+		this.tasksAtHand = (LinearLayout) findViewById(R.id.taskAtHandLinearLayout);
+		LayoutInflater inflater = (LayoutInflater) this.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View subHeader;
+		View taskPanel;
+		
+		subHeader = inflater.inflate(R.layout.sub_header_panel, null);
+		this.tasksAtHand.addView(subHeader);
+		
+//		Deploying multiple tasks.
+		for(int i = 0; i<3; i++) {
+			taskPanel = inflater.inflate(R.layout.task_panel, null);
+			this.tasksAtHand.addView(taskPanel);
+		}
 	}
 
 	@Override
