c7e31cd718c6cb2b5f6eb3309854d6f40d84e69b
Mahesh <mahesh@altersense.in>
2014-11-26 13:36:37 +0530

    android studio project finally set up right

--- src/android/TaskApp/app/src/main/java/in/altersense/taskapp/TasksActivity.java
+++ src/android/TaskApp/app/src/main/java/in/altersense/taskapp/TasksActivity.java
@@ -1,12 +1,13 @@
 package in.altersense.taskapp;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TasksActivity extends ActionBarActivity {
+public class TasksActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
