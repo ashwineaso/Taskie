9a58e89041f8d4f52416727960a871582ac2ba59
Mahesh Mohan <mahesh@altersense.in>
2015-01-20 17:21:36 +0530

    Each action button has its own action

--- src/android/TaskApp/app/src/main/java/in/altersense/taskapp/components/Task.java
+++ src/android/TaskApp/app/src/main/java/in/altersense/taskapp/components/Task.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import in.altersense.taskapp.R;
 import in.altersense.taskapp.customviews.CustomFontTextView;
@@ -20,8 +21,9 @@ public class Task {
     public boolean isActionsDisplayed;
     private int priority;
 
-    private View panelView;
+    private View panelView, actionsView;
     private LinearLayout taskActionsPlaceHolderView;
+    private LinearLayout action1, action2, action3, action4;
 
     /**
      * Create Task with Title, Description and Owner Name.
@@ -40,15 +42,63 @@ public class Task {
         this.descr = descr;
         this.ownerName = ownerName;
         this.panelView = createView(inflater);
+        this.actionsView = createActionsView(inflater);
         this.taskActionsPlaceHolderView =
                 (LinearLayout) this.panelView.findViewById(R.id.actionsPlaceHolderLinearLayout);
         this.isActionsDisplayed = false;
     }
 
+    private View createActionsView(LayoutInflater inflater) {
+        final LayoutInflater myInflater = inflater;
+        View actionsPanel = inflater.inflate(R.layout.task_actions, null);
+
+        action1 = (LinearLayout) actionsPanel.findViewById(R.id.action1);
+        action2 = (LinearLayout) actionsPanel.findViewById(R.id.action2);
+        action3 = (LinearLayout) actionsPanel.findViewById(R.id.action3);
+        action4 = (LinearLayout) actionsPanel.findViewById(R.id.action4);
+
+        action1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(myInflater.getContext(), "Action1", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        action2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(myInflater.getContext(), "Action2", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        action3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(myInflater.getContext(), "Action3", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        action4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(myInflater.getContext(), "Action4", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        return actionsPanel;
+    }
+
     public void showTaskActions(LayoutInflater inflater) {
         Log.i(TAG, "Reached showTaskActions");
-        View actionsPanel = inflater.inflate(R.layout.task_actions, null);
-        this.taskActionsPlaceHolderView.addView(actionsPanel);
+        this.taskActionsPlaceHolderView.addView(this.actionsView);
         this.isActionsDisplayed = true;
     }
 
