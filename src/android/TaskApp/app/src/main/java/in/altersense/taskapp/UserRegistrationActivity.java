a0f43eaf273317261d59e0b6c90176044b975a6d
Mahesh Mohan <mahesh@altersense.in>
2015-01-29 17:02:54 +0530

    Added a new activity for user registration

---
+++ src/android/TaskApp/app/src/main/java/in/altersense/taskapp/UserRegistrationActivity.java
@@ -0,0 +1,95 @@
+package in.altersense.taskapp;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+
+public class UserRegistrationActivity extends ActionBarActivity {
+
+    private EditText nameET;
+    private EditText emailET;
+    private EditText regPasswordET;
+    private Button regButton;
+    private ImageButton showPasswordButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //        Setting up calligraphy
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/roboto_slab_regular.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
+        setContentView(R.layout.activity_user_registration);
+
+        // Initializing views
+        this.nameET = (EditText) findViewById(R.id.regNameET);
+        this.emailET = (EditText) findViewById(R.id.regEmailET);
+        this.regPasswordET = (EditText) findViewById(R.id.regPasswordET);
+        this.regButton = (Button) findViewById(R.id.regButton);
+        this.showPasswordButton = (ImageButton) findViewById(R.id.regShowPassButton);
+
+        this.showPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageButton imageButton = (ImageButton) v;
+                int inputType = regPasswordET.getInputType();
+                if (inputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                    regPasswordET.setRawInputType(
+                            InputType.TYPE_TEXT_VARIATION_PASSWORD
+                    );
+                    imageButton.setImageResource(R.drawable.ic_hide_password);
+                } else {
+                    regPasswordET.setRawInputType(
+                        InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                    );
+                    imageButton.setImageResource(R.drawable.ic_action_showpassword);
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Calligraphy attached to new
+     * @param newBase
+     */
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_registration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
