fb1de55ea25722a0c05411d8cbf7caa03d0ce49a
Ashwin Easo <ashwineaso@gmail.com>
2015-03-28 14:51:21 +0530

    modified server to send multiple buzzes

--- src/server/apps/task/views.py
+++ src/server/apps/task/views.py
@@ -300,18 +300,18 @@ def buzzCollaborators():
 	for obj in jsonObj["data"]:
 		response = {}
 		data = {}
-		try:
-			taskObj.id = obj
-			if checkAccessTokenValid(taskObj) is True:
-				flag = bll.buzzCollaborators(taskObj)
-			if (flag):
-				response["status"] = RESPONSE_SUCCESS
-			else:
-				response["status"] = RESPONSE_FAILED
-		except Exception as e:
+		# try:
+		taskObj.id = obj
+		if checkAccessTokenValid(taskObj) is True:
+			flag = bll.buzzCollaborators(taskObj)
+		if (flag):
+			response["status"] = RESPONSE_SUCCESS
+		else:
 			response["status"] = RESPONSE_FAILED
-			response["message"] = str(e)
-			if hasattr(e, 'code'):
-				response["code"] = e.code
+		# except Exception as e:
+		# response["status"] = RESPONSE_FAILED
+		# response["message"] = str(e)
+		# if hasattr(e, 'code'):
+		# 	response["code"] = e.code
 		jsonResponse["data"].append(response)
 	return jsonResponse
