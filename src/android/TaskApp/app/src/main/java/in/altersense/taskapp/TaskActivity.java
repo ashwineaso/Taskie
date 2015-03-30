37f1a1fa05709eb7f44f48fbfe83e64ba567be8f
Ashwin Easo <ashwineaso@gmail.com>
2015-03-30 13:14:49 +0530

    Modifies layout of collaborator_details_view and task view

--- src/android/TaskApp/app/src/main/java/in/altersense/taskapp/TaskActivity.java
+++ src/android/TaskApp/app/src/main/java/in/altersense/taskapp/TaskActivity.java
@@ -93,7 +93,7 @@ public class TaskActivity extends ActionBarActivity {
         this.taskDescriptionTV.setText(this.task.getDescription());
         this.dueDateTV.setText(this.task.getDueDateTime());
         this.taskPriorityTV.setText(priorityToString(this.task.getPriority()));
-        this.taskStatusTV.setText("Status : " + statusToString(this.task.getStatus(getApplicationContext())));
+        this.taskStatusTV.setText(statusToString(this.task.getStatus(getApplicationContext())));
         this.taskOwnerTV.setText(this.task.getOwner().getName());
 
         //Fill the ArrayList with the required data
