7265b14da1eedce2f93fd0ce1e2a799d0a530dc4
Mahesh Mohan <mahesh@altersense.in>
2015-04-07 14:27:01 +0530

    Added back some missing code during deletion

--- src/server/TaskApp.py
+++ src/server/TaskApp.py
@@ -10,17 +10,15 @@ print 'ROOT:', PROJECT_ROOT
 from bottle import Bottle, debug, default_app
 from settings import routes
 
-
-
+# In case of WSGI execution the following gets executed
+TaskApp = Bottle()
+routes.set(TaskApp)
 
 debug(DEBUG)
 
-if __name__ == '__main__':
 # In case of execution from command line the following gets executed.
+if __name__ == '__main__':
 	TaskApp.run(host='0.0.0.0', port=8080, reloader=True)
 else:
-# In case of WSGI execution the following gets executed
-	TaskApp = Bottle()
 	TaskApp = default_app()
-
 routes.set(TaskApp)
