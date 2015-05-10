8eb34995e4372548a1e2613034c44a0bad089a88
Mahesh Mohan <mahesh@altersense.in>
2015-05-10 22:30:14 +0530

    added code to make the link working in url Text view

--- src/android/TaskApp/app/src/main/java/in/altersense/taskapp/AboutActivity.java
+++ src/android/TaskApp/app/src/main/java/in/altersense/taskapp/AboutActivity.java
@@ -3,12 +3,14 @@ package in.altersense.taskapp;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 
 public class AboutActivity extends AppCompatActivity {
 
     private TextView versionDisplay;
+    private TextView urlDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,9 @@ public class AboutActivity extends AppCompatActivity {
 
         this.versionDisplay = (TextView) findViewById(R.id.versionDisplayTV);
         this.versionDisplay.setText("Version: "+versionText);
+
+        this.urlDisplay = (TextView) findViewById(R.id.urlTV);
+        urlDisplay.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
