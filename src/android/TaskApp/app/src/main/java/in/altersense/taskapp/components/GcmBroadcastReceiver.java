2c7b51f666dc5112378774d357e55e52c1759a01
Mahesh Mohan <mahesh@altersense.in>
2015-06-09 11:09:58 +0530

    removed filtered false form manifest. checks for ordered and unordered broadcasts before handling them

--- src/android/TaskApp/app/src/main/java/in/altersense/taskapp/components/GcmBroadcastReceiver.java
+++ src/android/TaskApp/app/src/main/java/in/altersense/taskapp/components/GcmBroadcastReceiver.java
@@ -5,18 +5,28 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
 /**
  * Created by ashwineaso on 2/27/15.
  */
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+
+    private static final String CLASS_TAG = "GcmBroadcastReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        //explicitly specify that the GCM message handler will handle the intent
-        ComponentName componentName = new ComponentName(context.getPackageName(),GcmMessageHandler.class.getName());
-        //Start the service keeping the device awake while its launching
-        startWakefulService(context,(intent.setComponent(componentName)));
-        setResultCode(Activity.RESULT_OK);
+        if(isOrderedBroadcast()) {
+            Log.i(CLASS_TAG, "Received ordered broadcast.");
+            //explicitly specify that the GCM message handler will handle the intent
+            ComponentName componentName = new ComponentName(context.getPackageName(),GcmMessageHandler.class.getName());
+            //Start the service keeping the device awake while its launching
+            startWakefulService(context,(intent.setComponent(componentName)));
+            setResultCode(Activity.RESULT_OK);
+        } else {
+            // TODO Handle unordered broadcast.
+            Log.i(CLASS_TAG, "Received unordered broadcast.");
+        }
     }
 }
